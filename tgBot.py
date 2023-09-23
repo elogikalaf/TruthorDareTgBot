dbb83865ee76faf0b58e6f7cd3be4157277db27c
elogikalaf <96155440+elogikalaf@users.noreply.github.com>
2023-09-23 14:30:07 +0330

    Update tgBot.py
    hehe

--- tgBot.py
+++ tgBot.py
@@ -204,7 +204,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("6461317960:AAFXaR6GmDFCXc-iY17_EjRGEz3nGF5R3Sc").build()
+    application = Application.builder().token("token").build()
 
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
     conv_handler = ConversationHandler(
